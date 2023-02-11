f03163ee56abc094f4e758f3d45ef3e8f1ed4ff8
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-11 20:09:47 +0200

    +

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const picturesContainer = document.querySelector(".js-gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+picturesContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+picturesContainer.addEventListener("click", onpicturesContainerCkick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a> 
+      `;
+    })
+    .join("");
+}
+
+function onpicturesContainerCkick(e) {
+  e.preventDefault();
+  if (!e.target) {
+    return;
+  }
+  console.log(e.target);
+
+  //     const instance = basicLightbox.create(`
+  //     <img src= ${e.target.dataset.source} width="800" height="600">
+  //   `);
+
+  //     instance.show();
+}
