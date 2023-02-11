c0c89509fdd923b7947eaaa84d3f2e755c9137b6
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-11 16:08:15 +0200

    render-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+console.log(createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, description }) => {
+      return `<div class="gallery">
+      <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="small-image.jpg"
+            data-source="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
