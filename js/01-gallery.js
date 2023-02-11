2ff58041255c237097e418f5136426220e4ce4e0
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-11 16:55:21 +0200

    render-gallery+

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,18 +2,20 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 console.log(galleryItems);
 
-console.log(createGalleryMarkup(galleryItems));
+const picturesContainer = document.querySelector(".js-gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
+picturesContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 function createGalleryMarkup(galleryItems) {
   return galleryItems
-    .map(({ preview, description }) => {
-      return `<div class="gallery">
+    .map(({ preview, original, description }) => {
+      return `
       <div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
+        <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
-            src="small-image.jpg"
-            data-source="${preview}"
+            src="${preview}"
+            data-source="${original}"
             alt="${description}"
           />
         </a>
